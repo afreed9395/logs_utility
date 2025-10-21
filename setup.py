c2eda5cbd0dea0e795fc4d0b95a87e04426814cb
vandana Killari <vandana.killari@email.com>
2025-10-21 16:48:10 +0530

    initial changes

---
+++ setup.py
@@ -0,0 +1,12 @@
+
+from setuptools import setup,find_packages
+
+setup(
+    name="logging_util",
+    version="1.0.0",
+    description="Reusable python logging setup utility",
+    author="afreed",
+    packages = find_packages(),
+    python_requires = ">=3.7",
+)
+
